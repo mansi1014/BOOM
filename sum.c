6ba3e2b632e850d15c87b95ef62189e3db8b92e5
mansi1014 <merimaiya1008@gmail.com>
2020-12-13 12:57:41 +0530

    added sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main()
+ {
+   int a=10, b=10;
+ 
+   
+   printf(The sum of two numbers is :%d /n",a);
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", sum);
+ 
+   return(0);
+}
